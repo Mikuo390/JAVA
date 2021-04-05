ad303de26753a2abf8e57175bcff1fd142b38102
Mikuo390 <72961325+Mikuo390@users.noreply.github.com>
2021-04-05 09:17:41 +0800

    Create Tester.java

---
+++ JAVASE/Management Information System1.0/com.yjh.mis/Tester.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.xt.mis;
+
+import com.xt.mis.menu.*;
+/**
+ * @author 15159
+ *
+ */
+public class Tester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new LoginMenu();
+//		new RegisterMenu();
+//		new ExitMenu();
+//		new VerificationCode();
+//		new FunctionMenu();
+//		new userMenu();	
+//		new AddInfoMenu();		
+//		new CreateMenu();
+//		new DeleteMenu();
+//		new SearchInfoMenu();
+		
+//		EmployeeInfo e1 = new EmployeeInfo();
+//		System.out.println(e1);
+
+		
+//		InfoSaver.saveInfo();
+//		InfoReader.readInfo();
+
+	}
+
+}
