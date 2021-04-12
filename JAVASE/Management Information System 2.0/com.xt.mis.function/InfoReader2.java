8f66690fe735c03fbc5ae950dd26b3ec9ec7a04e
Mikuo390 <72961325+Mikuo390@users.noreply.github.com>
2021-04-12 20:56:54 +0800

    Create InfoReader2.java

---
+++ JAVASE/Management Information System 2.0/com.xt.mis.function/InfoReader2.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.xt.mis.function;
+
+import com.xt.mis.EmployeeInfo;
+import com.xt.mis.menu.*;
+import java.io.BufferedInputStream;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+import com.xt.mis.menu.FunctionMenu;
+
+/**
+ * @author 15159
+ *
+ */
+public class InfoReader2 {
+	
+	public static void readInfo2() {
+		for(EmployeeInfo em:InfoReader.list)
+		FunctionMenu.infoWindow.append(String.valueOf(em.toString2())+"\n");
+	}
+}
